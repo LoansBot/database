44b52177a7fa93b4124a68e472465df279539354
Timothy Moore <mtimothy984@gmail.com>
2020-02-09 18:53:29 -0800

    Finish helper skeleton for migration tests

--- tests/migrations/helper.py
+++ tests/migrations/helper.py
@@ -1,5 +1,12 @@
 """Shared functions for migration tests
 """
 
-def setup_connection() -> psycopg2.connection:
-    """
+
+def setup_connection():
+    """Create a psycopg2 connection to the postgres database"""
+    pass
+
+
+def teardown_connection(conn):
+    """Teardown the given psycopg2 connection"""
+    pass
