0a0f2bb9a245486cf764b795971f85178d257da1
Timothy Moore <mtimothy984@gmail.com>
2020-02-12 17:32:26 -0800

    Better support for missing values in settings

--- src/settings.py
+++ src/settings.py
@@ -3,11 +3,23 @@ import configparser
 import os
 
 
+EXPECTED_KEYS = [
+    'DATABASE_HOST'
+    'DATABASE_PORT'
+    'DATABASE_USER'
+    'DATABASE_PASSWORD'
+    'DATABASE_DBNAME'
+    'AWS_ACCESS_KEY'
+    'AWS_SECRET_KEY'
+    'AWS_S3_BUCKET'
+    'AWS_S3_FOLDER'
+]
+
 def load_settings():
     cfg = configparser.ConfigParser()
     cfg.read('settings.ini')
     cfg = cfg['DEFAULT']
-    for nm in list(cfg.keys()):
+    for nm in EXPECTED_KEYS:
         if os.environ.get(nm):
             cfg[nm] = os.environ[nm]
     return cfg
