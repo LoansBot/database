7e46b77aff19be39904787f5545a2f7d87b16627
Timothy Moore <mtimothy984@gmail.com>
2020-03-11 05:18:49 -0700

    Fix lint errors

--- src/display_schema.py
+++ src/display_schema.py
@@ -1,6 +1,6 @@
 """Shows the database schema on relevant tables."""
 from run_migrations import setup_connection
-from pypika import PostgreSQLQuery as Query, Schema, Parameter
+from pypika import PostgreSQLQuery as Query, Schema
 import os
 import settings
 
@@ -27,6 +27,9 @@ def main():
     db_pass = cfg['DATABASE_PASSWORD']
     db_name = cfg['DATABASE_DBNAME']
 
+    old_pg_pass = os.environ.get('PGPASSWORD')
+    os.environ['PGPASSWORD'] = db_pass
+
     for tbl in rows:
         print('=' * 50)
         print(f'DESCRIBE {tbl}')
@@ -35,6 +38,11 @@ def main():
             f'-U {db_user} -c "\\d+ {tbl}"'
         )
         print()
+
+    if old_pg_pass is not None:
+        os.environ['PGPASSWORD'] = old_pg_pass
+    else:
+        del os.environ['PGPASSWORD']
 
 
 if __name__ == '__main__':
