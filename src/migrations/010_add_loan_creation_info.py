afceeebb1cffb3ca67c8810e493f9ee49d8ac03e
Timothy Moore <mtimothy984@gmail.com>
2020-05-11 07:08:54 -0700

    Add loan creation info

---
+++ src/migrations/010_add_loan_creation_info.py
@@ -0,0 +1,32 @@
+"""Creates the X"""
+
+
+def up(conn, cursor):
+    cursor.execute(
+        '''
+        CREATE TABLE loan_creation_infos (
+            id SERIAL,
+            loan_id INTEGER UNIQUE NOT NULL REFERENCES loans(id) ON DELETE CASCADE,
+            type INTEGER NOT NULL,
+            parent_fullname TEXT NULL,
+            comment_fullname TEXT NULL,
+            mod_user_id INTEGER NULL REFERENCES users(id) ON DELETE SET NULL
+        )
+        '''
+    )
+    print(cursor.query.decode('utf-8'))
+
+    cursor.execute(
+        '''
+        CREATE INDEX index_loan_creation_infos_on_mod_user_id
+            ON loan_creation_infos(mod_user_id)
+        '''
+    )
+    print(cursor.query.decode('utf-8'))
+
+
+def down(conn, cursor):
+    cursor.execute(
+        'DROP TABLE loan_creation_infos'
+    )
+    print(cursor.query.decode('utf-8'))
---
+++ tests/migrations/010_add_loan_creation_info_up.py
@@ -0,0 +1,27 @@
+import unittest
+import helper
+
+
+class UpTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.connection = helper.setup_connection()
+        cls.cursor = cls.connection.cursor()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.cursor.close()
+        cls.connection.rollback()
+        helper.teardown_connection(cls.connection)
+
+    def tearDown(self):
+        self.connection.rollback()
+
+    def test_loan_creation_infos_exists(self):
+        self.assertTrue(
+            helper.check_if_table_exist(self.cursor, 'loan_creation_infos')
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
