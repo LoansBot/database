3187e3f3a2cc58a71f9cbd2605913c788626eebd
Timothy Moore <mtimothy984@gmail.com>
2020-07-01 08:29:45 -0700

    Add password authentication history

---
+++ src/migrations/011_add_passauth_history.py
@@ -0,0 +1,40 @@
+"""Creates the X"""
+
+
+def up(conn, cursor):
+    cursor.execute(
+        '''
+        CREATE TABLE password_authentication_events (
+            id SERIAL,
+            password_authentication_id INTEGER NOT NULL REFERENCES password_authentications(id) ON DELETE CASCADE,
+            type TEXT NOT NULL,
+            reason TEXT NOT NULL,
+            user_id INTEGER NULL REFERENCES users(id) ON DELETE SET NULL,
+            created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+        )
+        '''
+    )
+    print(cursor.query.decode('utf-8'))
+
+    cursor.execute(
+        '''
+        CREATE INDEX idx_pass_auth_events_on_pass_auth_id
+            ON password_authentication_events(password_authentication_id)
+        '''
+    )
+    print(cursor.query.decode('utf-8'))
+
+    cursor.execute(
+        '''
+        CREATE INDEX idx_pass_auth_events_on_user_id
+            ON password_authentication_events(user_id)
+        '''
+    )
+    print(cursor.query.decode('utf-8'))
+
+
+def down(conn, cursor):
+    cursor.execute(
+        'DROP TABLE password_authentication_events CASCADE'
+    )
+    print(cursor.query.decode('utf-8'))
