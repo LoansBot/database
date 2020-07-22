c647c0f556caa2020bb9384aeb5829bc3e8b954b
Timothy Moore <mtimothy984@gmail.com>
2020-07-22 08:18:28 -0700

    Fix non-postgres add primary key sql

--- src/migrations/016_add_missing_pkeys.py
+++ src/migrations/016_add_missing_pkeys.py
@@ -4,21 +4,24 @@
 def up(conn, cursor):
     cursor.execute(
         '''
-        ALTER TABLE loan_creation_infos ADD PRIMARY KEY (id)
+        ALTER TABLE loan_creation_infos
+            ADD CONSTRAINT loan_creation_infos_pk PRIMARY KEY (id)
         '''
     )
     print(cursor.query.decode('utf-8'))
 
     cursor.execute(
         '''
-        ALTER TABLE password_authentication_events ADD PRIMARY KEY (id)
+        ALTER TABLE password_authentication_events
+            ADD CONSTRAINT password_authentication_events_pk PRIMARY KEY (id)
         '''
     )
     print(cursor.query.decode('utf-8'))
 
     cursor.execute(
         '''
-        ALTER TABLE user_settings_events ADD PRIMARY KEY (id)
+        ALTER TABLE user_settings_events
+            ADD CONSTRAINT user_settings_events_pk PRIMARY KEY (id)
         '''
     )
     print(cursor.query.decode('utf-8'))
@@ -27,21 +30,21 @@ def up(conn, cursor):
 def down(conn, cursor):
     cursor.execute(
         '''
-        ALTER TABLE user_settings_events DROP PRIMARY KEY
+        ALTER TABLE user_settings_events DROP CONSTRAINT loan_creation_infos_pk
         '''
     )
     print(cursor.query.decode('utf-8'))
 
     cursor.execute(
         '''
-        ALTER TABLE password_authentication_events DROP PRIMARY KEY
+        ALTER TABLE password_authentication_events DROP CONSTRAINT password_authentication_events_pk
         '''
     )
     print(cursor.query.decode('utf-8'))
 
     cursor.execute(
         '''
-        ALTER TABLE loan_creation_infos DROP PRIMARY KEY
+        ALTER TABLE user_settings_events DROP CONSTRAINT user_settings_events_pk
         '''
     )
     print(cursor.query.decode('utf-8'))
