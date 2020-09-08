c5ae3c8cd68863aa3848a4b0a1b3cd8461c4be9c
Timothy Moore <mtimothy984@gmail.com>
2020-09-08 07:08:05 -0700

    Add http verb to endpoints

---
+++ src/migrations/025_add_verb_to_endpoints.py
@@ -0,0 +1,51 @@
+"""Creates the X"""
+
+
+def up(conn, cursor):
+    cursor.execute(
+        '''
+        DROP INDEX endpoints_path_key RESTRICT
+        '''
+    )
+    print(cursor.query.decode('utf-8'))
+
+    cursor.execute(
+        '''
+        ALTER TABLE endpoints
+            ADD COLUMN verb TEXT NOT NULL DEFAULT 'GET'
+        '''
+    )
+    print(cursor.query.decode('utf-8'))
+
+    cursor.execute(
+        '''
+        CREATE UNIQUE INDEX index_endpoints_on_path_and_verb
+            ON endpoints(path, verb)
+        '''
+    )
+    print(cursor.query.decode('utf-8'))
+
+
+def down(conn, cursor):
+    cursor.execute(
+        '''
+        DROP INDEX index_endpoints_on_path_and_verb RESTRICT
+        '''
+    )
+    print(cursor.query.decode('utf-8'))
+
+    cursor.execute(
+        '''
+        ALTER TABLE endpoints
+            DROP COLUMN verb
+        '''
+    )
+    print(cursor.query.decode('utf-8'))
+
+    cursor.execute(
+        '''
+        CREATE UNIQUE INDEX endpoints_path_key
+            ON endpoints(path)
+        '''
+    )
+    print(cursor.query.decode('utf-8'))
